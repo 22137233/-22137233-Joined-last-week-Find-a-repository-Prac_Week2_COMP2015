2a6a475ffa0b6e199b5a46e929a71ddf9892aa09
22137233 <22137233@students.atmc.edu.au>
2024-08-08 16:00:08 +1000

    Add files via upload

---
+++ pracweek3-22137233/README.md
@@ -0,0 +1,9 @@
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/0mQHN8Dl)
+# PT-Week3-Prac
+
+This repo contains 2 java files for exercise 1.
+
+- Clone the repo to your Eclipse workspace on your **local** drive as described in the Week 3 practical pdf document.
+- Work locally on your solutions for each Exercise, committing and pushing on a regular basis as you progress through each solution.
+- Ensure that you commit and push at the end of your practical class so we know the progress you have made during class time.
+- When completed, perform your final commit and push prior to the due date for the week 3 exercises.
---
+++ pracweek3-22137233/src/dataStreams1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class dataStreams1 {
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        byte number1;
+        byte number2;
+        float number3;
+        String message;
+
+        System.out.println("Please enter a whole number");
+        number1 = kb.nextByte();
+        kb.nextLine(); 
+
+        System.out.println("Please enter another whole number");
+        number2 = kb.nextByte();
+        kb.nextLine();
+
+        System.out.println("Please enter a fractional number");
+        number3 = kb.nextFloat();
+        kb.nextLine(); 
+
+        System.out.println("Please enter a message");
+        message = kb.nextLine();
+
+      
+        System.out.println("Data entered was:");
+        System.out.println("number 1: " + number1);
+        System.out.println("number 2: " + number2);
+        System.out.println("number 3: " + number3);
+        System.out.println("message: " + message);
+        
+       //To let user to close there data
+       System.out.println("Press Enter to quite this...");
+        kb.nextLine();
+
+        kb.close();  // Close the scanner when done
+    }
+}
